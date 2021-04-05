5cb5a9b749a45f133328fab564f3b9d97c6b4cc5
eelopez515 <eelopez15@gmail.com>
2021-04-05 03:36:01 -0400

    made some layout changes to homepage to include the MainEvent component

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import './Homepage.sass';
+// Components
+import MainEvent from '../MainEvent/MainEvent';
 
 function Homepage({ history }) {
   return (
     <div className="homepage">
-      <div className="homepage__covid__info">
-        <p onClick={() => history.push('/covid')}>
-          Evaluate COVID-19 symptoms and undertand next steps
-        </p>
+      <div className="homepage__header">
+        <div className="homepage__covid__info">
+          <p onClick={() => history.push('/covid')}>
+            Evaluate COVID-19 symptoms and undertand next steps
+          </p>
+        </div>
+        <div className="homepage__shop__online">
+          <p>
+            <span
+              onClick={() => history.push('/shop')}
+              className="shop__online"
+            >
+              Shop online
+            </span>{' '}
+            and get Specialist help, free no-contact delivery, and more.
+          </p>
+        </div>
       </div>
-      <div className="homepage__shop__online">
-        <p>
-          <span onClick={() => history.push('/shop')} className="shop__online">
-            Shop online
-          </span>{' '}
-          and get Specialist help, free no-contact delivery, and more.
-        </p>
+      <div className="homepage__contents">
+        <MainEvent />
+        <MainEvent />
+        <MainEvent />
       </div>
     </div>
   );
---
+++ src/Components/MainEvent/MainEvent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainEvent.sass';
+
+function MainEvents() {
+  return (
+    <div className="mainEvent">
+      <h1>Main Event</h1>
+    </div>
+  );
+}
+
+export default MainEvents;
